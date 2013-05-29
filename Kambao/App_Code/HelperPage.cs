587402b10ba436a9639cafd6d393f2b5ebffe1fe
Elvis Lima <elvisdelima89@gmail.com>
2013-05-29 19:07:09 -0300

    Menu bootstrap e ajuste no modal de inclusão de faixas

---
+++ Kambao/App_Code/HelperPage.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.WebPages;
+
+namespace Kambao.App_Code
+{
+    public class HelperPage : System.Web.WebPages.HelperPage
+    {
+        public static new HtmlHelper Html
+        {
+            get { return ((System.Web.Mvc.WebViewPage)WebPageContext.Current.Page).Html; }
+        }
+    }
+}
